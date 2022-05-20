1f3912ff82d621620e7d98298dd031f308020e67
Xianglong Tan <xianglong.tan@capgemini.com>
2022-05-21 00:42:28 +0800

    Added Posts Index View

---
+++ Blog/Controllers/PostsController.cs
@@ -0,0 +1,24 @@
+﻿using Blog.Models;
+using Blog.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly IPostRepository _postRepository;
+
+        [TempData]
+        public string message { get; set; }
+
+        public PostsController(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+        public IActionResult Index(string searchString)
+        {
+            ViewData["CurrentFilter"] = searchString;
+            return View();
+        }
+    }
+}
