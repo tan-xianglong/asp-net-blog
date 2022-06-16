760163f480379c14318187d4cf0b1fc59fd40f86
Xianglong Tan <xianglong.tan@capgemini.com>
2022-06-16 23:23:24 +0800

    Added postControllerWebApiTests

---
+++ BlogTest/WebApiTests/PostControllerWebApiTests.cs
@@ -0,0 +1,87 @@
+﻿using Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Controllers;
+using WebAPI.Services;
+using WebAPI.ViewModels;
+using Xunit;
+
+namespace BlogTest.WebApiTests
+{
+    public class PostControllerWebApiTests
+    {
+        private PostController CreateDefaultPostController()
+        {
+            var postServicesMock = Substitute.For<IPostServicesWebApi>();
+            postServicesMock.GetPostViewModelAsync(1)
+                .Returns(Task.FromResult(
+                    new PostViewModel
+                    {
+                        PostId = 1,
+                        Content = "Lorem Ipsum",
+                        Title = "Test 1",
+                        Subtitle = "test",
+                        CreateDate = DateTime.Now,
+                        Comments = new List<Comment>
+                        {
+                            new Comment()
+                            {
+                                CommentId = 1,
+                                Author = "Jack",
+                                Body = "Lorem Ipsum",
+                                Email = "a@a.com",
+                                CreateDate= DateTime.Now,
+                                PostId = 1
+                            },
+                            new Comment()
+                            {
+                                CommentId = 2,
+                                Author = "John",
+                                Body = "Lorem Ipsum",
+                                Email = "a@a.com",
+                                CreateDate= DateTime.Now,
+                                PostId = 1
+                            }
+                        }
+                    }));
+            return new PostController(postServicesMock);
+        }
+
+        [Fact]
+        public async Task GetPost_ValidPostId_MustReturnOkWithPostViewModel()
+        {
+            //Arrange
+            var postController = CreateDefaultPostController();
+            var validPostId = 1;
+
+            //Act
+            var result = await postController.GetPost(validPostId);
+
+            //Assert
+            var actionResult = Assert.IsType<ActionResult<PostViewModel>>(result);
+
+            Assert.IsType<OkObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async Task GetPost_InValidPostId_MustReturnNotFound()
+        {
+            //Arrange
+            var postController = CreateDefaultPostController();
+            var invalidPostId = 2;
+
+            //Act
+            var result = await postController.GetPost(invalidPostId);
+
+            //Assert
+            var actionResult = Assert.IsType<ActionResult<PostViewModel>>(result);
+
+            Assert.IsType<NotFoundResult>(actionResult.Result);
+        }
+    }
+}
